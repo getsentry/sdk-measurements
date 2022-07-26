7f9dc23f83c9833d60c0def0e81567231e98ae7d
Anton Ovchinnikov <anton@tonyo.info>
2022-07-26 14:20:08 +0200

    connect everything

--- platforms/python/django-profiling/hello/hello/settings.py
+++ platforms/python/django-profiling/hello/hello/settings.py
@@ -9,7 +9,7 @@ sentry_sdk.init(
     # debug=True,
 )
 
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = '_7mb6#v4yf@qhc(r(zbyh&amp;z_iby-na*7wz&amp;-v6pohsul-d#y5f'
 ADMINS = ()
@@ -22,8 +22,8 @@ DATABASES = {
         'NAME': 'hello_world',           # Or path to database file if using sqlite3.
         'USER': 'benchmarkdbuser',       # Not used with sqlite3.
         'PASSWORD': 'benchmarkdbpass',   # Not used with sqlite3.
-        'HOST': 'tfb-database',  # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'HOST': '127.0.0.1',             # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                  # Set to empty string for default. Not used with sqlite3.
         'CONN_MAX_AGE': 30,
     }
 }
---
+++ platforms/python/django-profiling/test-run-vegeta-disable-profiling.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+export TARGET_BASE="http://127.0.0.1:8090"
+
+. ./_test-run-vegeta-common.sh
