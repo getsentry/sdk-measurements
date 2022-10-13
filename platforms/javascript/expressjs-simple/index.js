d9539e1165477eac309b62c7e99b9d017a5dc0a0
Anton Ovchinnikov <anton@tonyo.info>
2022-10-13 20:01:51 +0200

    update again

--- platforms/javascript/expressjs-simple/index.js
+++ platforms/javascript/expressjs-simple/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.SENTRY_PORT_BACKEND
+const port = process.env.SENTRY_PORT_BACKEND || 8081
+
+process.once('SIGTERM', function () {
+  console.log('got SIGTERM');
+  process.exit(0);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello Anton & Radu!')
