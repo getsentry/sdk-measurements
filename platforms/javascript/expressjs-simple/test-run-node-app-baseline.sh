0f35d468aab0756b921da6b417dd246f2ee3613e
Anton Ovchinnikov <anton@tonyo.info>
2022-10-13 15:51:23 +0200

    try to fix

--- platforms/javascript/expressjs-simple/test-run-node-app-baseline.sh
+++ platforms/javascript/expressjs-simple/test-run-node-app-baseline.sh
@@ -1,1 +1,3 @@
-vegeta attack -targets ./vegeta-requests.txt -rate=80 -duration=10s | vegeta encode
+# vegeta attack -targets ./vegeta-requests.txt -rate=80 -duration=10s | vegeta encode
+echo test
+sleep 1
