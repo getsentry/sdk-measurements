b45abb257f3d24a38bd9ca17d631d912c468f851
Anton Ovchinnikov <anton@tonyo.info>
2022-10-14 10:34:56 +0200

    Fixes to make Argo happy

--- platforms/javascript/expressjs-simple/index.js
+++ platforms/javascript/expressjs-simple/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
-const port = process.env.SENTRY_PORT_BACKEND
+const port = process.env.SENTRY_PORT_BACKEND || 8081
+
+process.once('SIGTERM', function () {
+  console.log('got SIGTERM, exiting...');
+  // Exit code is 0 to make Argo runtime happy
+  process.exit(0);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello Anton & Radu!')
