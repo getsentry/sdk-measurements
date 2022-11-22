f696de2cad29a30f4a96052ec61a739f499060da
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2022-11-22 15:26:58 +0100

    fix custom data file name

--- platforms/javascript/expressjs-simple/test-run-node-app-baseline.sh
+++ platforms/javascript/expressjs-simple/test-run-node-app-baseline.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 export TARGET_BASE="http://127.0.0.1:3000"
-echo '{"labels":[{"name":"baseTest", "value":"true"},{"name":"displayName", "value":"Base"}]}' > /tmp/test_data.json
+echo '{"labels":[{"name":"baseTest", "value":"true"},{"name":"displayName", "value":"Base"}]}' > /tmp/custom-data.json
 
 . _test-run-common.sh
